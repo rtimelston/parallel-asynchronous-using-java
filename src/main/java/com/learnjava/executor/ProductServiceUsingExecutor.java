0acb79590ab1a4276d6840fc07e8412837e8dd25
rtimelston <13246079+rtimelston@users.noreply.github.com>
2021-06-14 16:45:01 -0700

    Section 5.12 ExecutorService with Future

---
+++ src/main/java/com/learnjava/executor/ProductServiceUsingExecutor.java
@@ -0,0 +1,61 @@
+package com.learnjava.executor;
+
+import com.learnjava.domain.Product;
+import com.learnjava.domain.ProductInfo;
+import com.learnjava.domain.Review;
+import com.learnjava.service.ProductInfoService;
+import com.learnjava.service.ReviewService;
+
+import java.util.concurrent.*;
+
+import static com.learnjava.util.CommonUtil.stopWatch;
+import static com.learnjava.util.LoggerUtil.log;
+
+/**
+ * Exemplifies using ExecutorService to run parallel tasks.
+ *
+ * This is more elegant than using Thread objects as in {@link com.learnjava.thread.ProductServiceUsingThread}
+ * because Runnable implementations do not have to be created.
+ *
+ * But the {@link Future#get()} call is blocking.
+ */
+public class ProductServiceUsingExecutor {
+
+    private static ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private ProductInfoService productInfoService;
+    private ReviewService reviewService;
+
+    public ProductServiceUsingExecutor(ProductInfoService productInfoService, ReviewService reviewService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+    }
+
+    public Product retrieveProductDetails(String productId) throws ExecutionException, InterruptedException, TimeoutException {
+        stopWatch.start();
+
+        Future<ProductInfo> productInfoFuture = executorService.submit(() -> productInfoService.retrieveProductInfo(productId));
+        Future<Review> reviewFuture = executorService.submit(() -> reviewService.retrieveReviews(productId));
+        // Without timeouts
+//        ProductInfo productInfo = productInfoFuture.get();
+//        Review review = reviewFuture.get();
+        // With timeouts
+        ProductInfo productInfo = productInfoFuture.get(2, TimeUnit.SECONDS);
+        Review review = reviewFuture.get(2, TimeUnit.SECONDS);
+
+        stopWatch.stop();
+        log("Total Time Taken : "+ stopWatch.getTime());
+        return new Product(productId, productInfo, review);
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+
+        ProductInfoService productInfoService = new ProductInfoService();
+        ReviewService reviewService = new ReviewService();
+        ProductServiceUsingExecutor productService = new ProductServiceUsingExecutor(productInfoService, reviewService);
+        String productId = "ABC123";
+        Product product = productService.retrieveProductDetails(productId);
+        log("Product is " + product);
+
+        executorService.shutdown();
+    }
+}
